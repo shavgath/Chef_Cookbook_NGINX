f71919afb46032f2227b424fc3a48c680155e59e
Shavgath Perumal <48409809+shavgath@users.noreply.github.com>
2019-08-18 19:35:46 +0100

    Completed nginx cookbook

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook:: nginx
+# Cookbook:: node
 # Spec:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
@@ -8,22 +8,43 @@ require 'spec_helper'
 
 describe 'nginx::default' do
   context 'When all attributes are default, on Ubuntu 18.04' do
+    let(:chef_run) do
+    runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
+    runner.converge(described_recipe)
+    end
     # for a complete list of available platforms and versions see:
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'ubuntu', '18.04'
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
+    it 'should install nginx' do
+      expect(chef_run).to install_package "nginx"
+    end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'should enable the nginx service' do
+      expect(chef_run).to enable_service "nginx"
+    end
+
+    it 'start the nginx service' do
+      expect(chef_run).to start_service "nginx"
+    end
+
+    it 'should create a proxy.conf template in /etc/nginx/sites-available' do
+      expect(chef_run).to create_template("/etc/nginx/sites-available/proxy.conf").with_variables(proxy_port: 3000)
+    end
+
+    it 'should create a symlink from nginx/sites-available to nginx sites-available' do
+      expect(chef_run).to create_link("/etc/nginx/sites-enabled/proxy.conf").with_link_type(:symbolic)
+    end
+
+    it 'should delete the symlink from the default config in sites-enabled' do
+      expect(chef_run).to delete_link("/etc/nginx/sites-enabled/default")
+    end
+
+    it 'runs apt get update' do
+      expect(chef_run).to update_apt_update "update_sources"
     end
   end
 end
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,16 +1,19 @@
-# InSpec test for recipe nginx::default
+# InSpec test for recipe node::default
 
 # The InSpec reference, with examples and extensive documentation, can be
 # found at https://www.inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+# This is an example test, replace it with your own test.
+describe service ('nginx') do
+  it {should be_installed}
+  it {should be_enabled}
+  it {should be_running}
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe port(80) do
+  it { should be_listening }
+end
+
+describe http('http://localhost', enable_remote_worker: true) do
+  its('status'){ should cmp 502 }
 end
