d91776a4d99a441f81737df62c49c2edbf3e5c92
Shavgath Perumal <48409809+shavgath@users.noreply.github.com>
2019-08-19 11:14:27 +0100

    Modified the recipe file to disable restart

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,7 +15,7 @@ end
 template "/etc/nginx/sites-available/proxy.conf" do
   source "proxy.conf.erb"
   variables proxy_port: node['nginx']['proxy_port']
-  notifies :restart, 'service[nginx]'
+  # notifies :restart, 'service[nginx]'
 end
 
 link "/etc/nginx/sites-enabled/proxy.conf" do
